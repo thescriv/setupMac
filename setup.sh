32089b0d2972340f5552fd731dd4c2dfb7ac4483
theo esc <theoesc@mbp-de-theo.home>
2022-07-04 15:51:49 +0200

    init

---
+++ setup.sh
@@ -0,0 +1,21 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew update
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+brew install docker
+brew install --cask spotify
+brew install --cask visual-studio-code
+brew install --cask docker
+brew install --cask robo-3t
+brew install --cask slack
+brew install --cask messenger
+brew install --cask whatsapp
+brew install --cask discord
+brew install --cask notion
+brew install node
+brew install nvm
+brew install npm
+npm -g tsc
+npm -g nodemon
