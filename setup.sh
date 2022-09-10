0f87921ec253f29e3c2d6cc7d61c133e13bcc5b5
thescriv <thescriv@student.42.fr>
2022-09-10 12:38:19 +0200

    added env var for brew

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/theolatournee/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew update
 
